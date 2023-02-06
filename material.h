701c0ed61b9fddf3644cab5e17b81864b8dabf74
Allan Gelman <allangelman4@gmail.com>
2023-02-05 19:15:32 -0500

    glass

--- material.h
+++ material.h
@@ -55,4 +55,34 @@ class metal : public material {
         double fuzz;
 };
 
+class dielectric : public material {
+    public:
+        dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+        virtual bool scatter(
+            const ray& r_in, const hit_data& data, color& attenuation, ray& scattered
+        ) const override {
+            attenuation = color(1.0, 1.0, 1.0);
+            double refraction_ratio = data.front_face ? (1.0/ir) : ir;
+
+            vec3 unit_direction = unit_vector(r_in.direction());
+            double cos_theta = fmin(dot(-unit_direction, data.hit_normal), 1.0);
+            double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+            bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+            vec3 direction;
+
+            if (cannot_refract)
+                direction = reflect(unit_direction, data.hit_normal);
+            else
+                direction = refract(unit_direction, data.hit_normal, refraction_ratio);
+
+            scattered = ray(data.hit_point, direction);
+            return true;
+        }
+
+    public:
+        double ir; // Index of Refraction
+};
+
 #endif
