380c6c74a47ffa1214d75b43547e5a89c2539ba1
Allan Gelman <allangelman4@gmail.com>
2023-02-11 00:09:07 -0500

    applied bvh to original scene

--- main.cpp
+++ main.cpp
@@ -36,6 +36,59 @@ color ray_color(const ray& r, const hittable& world,  int depth) {
     return (1.0-t)*color(1.0, 1.0, 1.0) + t*color(0.5, 0.7, 1.0);
 }
 
+ //TODO FOR ME!!!!!:  make one hittable list that is called boxes and add all the spheres to that
+ // THEN MAKE A BVH NODE OUT OF THAT LIST? AND ADD IT TO THE WORLD
+
+
+hittable_list random_scene_bvh() {
+    hittable_list world;
+    hittable_list boxes;
+
+    auto ground_material = make_shared<lambertian>(color(0.5, 0.5, 0.5));
+    boxes.add(make_shared<sphere>(point3(0,-1000,0), 1000, ground_material));
+
+    for (int a = -11; a < 11; a++) {
+        for (int b = -11; b < 11; b++) {
+            auto choose_mat = random_double();
+            point3 center(a + 0.9*random_double(), 0.2, b + 0.9*random_double());
+
+            if ((center - point3(4, 0.2, 0)).length() > 0.9) {
+                shared_ptr<material> sphere_material;
+
+                if (choose_mat < 0.8) {
+                    // diffuse
+                    auto albedo = color::random() * color::random();
+                    sphere_material = make_shared<lambertian>(albedo);
+                    boxes.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else if (choose_mat < 0.95) {
+                    // metal
+                    auto albedo = color::random(0.5, 1);
+                    auto fuzz = random_double(0, 0.5);
+                    sphere_material = make_shared<metal>(albedo, fuzz);
+                    boxes.add(make_shared<sphere>(center, 0.2, sphere_material));
+                } else {
+                    // glass
+                    sphere_material = make_shared<dielectric>(1.5);
+                    boxes.add(make_shared<sphere>(center, 0.2, sphere_material));
+                }
+            }
+        }
+    }
+
+    auto material1 = make_shared<dielectric>(1.5);
+    boxes.add(make_shared<sphere>(point3(0, 1, 0), 1.0, material1));
+
+    auto material2 = make_shared<lambertian>(color(0.4, 0.2, 0.1));
+    boxes.add(make_shared<sphere>(point3(-4, 1, 0), 1.0, material2));
+
+    auto material3 = make_shared<metal>(color(0.7, 0.6, 0.5), 0.0);
+    boxes.add(make_shared<sphere>(point3(4, 1, 0), 1.0, material3));
+
+    world.add(make_shared<bvh_node>(boxes, 0.0, 1.0));
+
+    return world;
+}
+
 hittable_list random_scene() {
     hittable_list world;
 
@@ -127,7 +180,7 @@ int main() {
     // Image
     // const auto aspect_ratio = 16.0 / 9.0;
     const auto aspect_ratio = 3.0 / 2.0;
-    const int image_width = 400;
+    const int image_width = 100;
 
     // const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
@@ -151,8 +204,10 @@ int main() {
 
     // hittable_list world;
 
-    // auto world = random_scene();
-    auto world = bvh_test_scene();
+    auto world = random_scene();
+    // auto world = random_scene_bvh();
+    // auto world = bvh_test_scene();
+
 
     // auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
     // auto material_center = make_shared<lambertian>(color(0.1, 0.2, 0.5));
@@ -176,20 +231,20 @@ int main() {
     // auto aperture = 2.0;
 
     // SPHERE SCENE
-    // point3 lookfrom(13,2,3);
-    // point3 lookat(0,0,0);
-    // vec3 vup(0,1,0);
-    // auto dist_to_focus = 10.0;
-    // auto aperture = 0.1;
-    // auto vfov = 20.0;
-
-    // BVH SCENE
-    point3 lookfrom(478, 278, -600);
-    point3 lookat(278, 278, 0);
+    point3 lookfrom(13,2,3);
+    point3 lookat(0,0,0);
     vec3 vup(0,1,0);
     auto dist_to_focus = 10.0;
     auto aperture = 0.1;
-    auto vfov = 40.0;
+    auto vfov = 20.0;
+
+    // BVH SCENE
+    // point3 lookfrom(478, 278, -600);
+    // point3 lookat(278, 278, 0);
+    // vec3 vup(0,1,0);
+    // auto dist_to_focus = 10.0;
+    // auto aperture = 0.1;
+    // auto vfov = 40.0;
 
 
     camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
