61eeec267227512d13886b2602bb98789035b45b
Allan Gelman <allangelman4@gmail.com>
2023-03-19 02:17:48 -0400

    last image

--- main.cpp
+++ main.cpp
@@ -101,6 +101,50 @@ hittable_list bunny() {
     return world;
 }
 
+hittable_list shapes() {
+    hittable_list objects;
+    hittable_list world;
+    mesh m;
+
+    auto red   = make_shared<lambertian>(color(.55, .15, .25));
+    auto white = make_shared<lambertian>(color(.73, .73, .73));
+    auto green = make_shared<lambertian>(color(.27, .51, .71));
+    auto orange = make_shared<lambertian>(color(1.0, .29, .31));
+    auto purple = make_shared<lambertian>(color(0.28, .24, .55));
+    auto light = make_shared<diffuse_light>(color(15, 15, 15));
+    auto purplelight = make_shared<diffuse_light>(color(3, 7, 10));
+    auto glass = make_shared<dielectric>(1.5);
+    auto metalic = make_shared<metal>(color(.72, .45, .2), 0.3);
+    auto yellow = make_shared<lambertian>(color(.72, .45, .2));
+    auto mirrormat = make_shared<mirror>(color(.30, .05, .23));
+
+    objects.add(make_shared<xz_rect>(163, 393, 777, 982, 554, light));
+    // objects.add(make_shared<xy_rect>(300, 400, 200, 300, -100, purplelight));
+
+    objects.add(make_shared<yz_rect>(0, 555, 0, 1500, 555, purple));
+    objects.add(make_shared<yz_rect>(0, 555, 0, 1500, 0, mirrormat));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 1500, 0, white));
+    objects.add(make_shared<xz_rect>(0, 555, 0, 1500, 555, white));
+    objects.add(make_shared<xy_rect>(0, 555, 0, 555, 1500, yellow));
+
+    objects.add(make_shared<sphere>(vec3(100,270,600), 55, metalic));
+    objects.add(make_shared<box>(vec3(40,0,540), vec3(160,215,660), red));
+
+    objects.add(make_shared<sphere>(vec3(260,390,930), 40, glass));
+    shared_ptr<hittable> box1 = make_shared<box>(vec3(200,0,870), vec3(320,350,990), metalic);
+    // box1 = make_shared<translate>(box1, vec3(1,0,1));
+    // box1 = make_shared<rotate_y>(box1, 50.0);
+    objects.add(box1);
+
+
+    objects.add(make_shared<sphere>(vec3(330,310,200), 70, glass));
+    objects.add(make_shared<box>(vec3(250,0,120), vec3(410,240,280), orange));
+
+    world.add(make_shared<bvh_node>(objects, 0.0, 1.0));
+
+    return world;
+}
+
 hittable_list cornell_box() {
     hittable_list objects;
     hittable_list world;
@@ -141,7 +185,7 @@ int main() {
     // const auto aspect_ratio = 1.0;
     const int image_width = 600;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
-    const int samples_per_pixel = 1300;
+    const int samples_per_pixel = 3000;
     const int max_depth = 25;
     color background(0,0,0);
 
@@ -164,12 +208,23 @@ int main() {
     // auto vfov = 50.0;
 
     //bunny
-    auto world = bunny();
-    auto lookfrom = point3(265, 450, -200);
-    auto lookat = point3(310, 380, 200);
+    // auto world = bunny();
+    // auto lookfrom = point3(265, 450, -200);
+    // auto lookat = point3(310, 380, 200);
+    // vec3 vup(0,1,0);
+    // auto dist_to_focus = 1088.63;
+    // auto aperture = 0.2;
+    // auto vfov = 40.0;
+
+    // shapes
+    auto world = shapes();
+    auto lookfrom = point3(265, 450, -250);
+    auto lookat = point3(180, 350, 200);
     vec3 vup(0,1,0);
-    auto dist_to_focus = 1088.63;
-    auto aperture = 0.2;
+    // auto dist_to_focus = 1181.53;
+    auto dist_to_focus = 884.37;
+    // auto dist_to_focus = 10.0;
+    auto aperture = 30.0;
     auto vfov = 40.0;
 
     camera cam(lookfrom, lookat, vup, vfov, aspect_ratio, aperture, dist_to_focus);
